20755c95844598dc41751d2c8c3bb268e8c624fa
herreraflores <herrflor10@gmail.com>
2022-10-29 15:18:00 -0700

    Changed text
    .

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>First commit.</Text>
+      <Text>Recieved changes.</Text>
       <StatusBar style="auto" />
     </View>
   );
